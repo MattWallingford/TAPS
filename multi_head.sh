22f9b9a685340f56bfdd23e7ef07f3916bc07aa3
MattWallingford <mcw244@cornell.edu>
2022-10-11 11:59:45 -0700

    adding files

--- multi_head.sh
+++
@@ -1,4 +0,0 @@
-
-for learn in  .01 .005; do
-    python fine_tune_multi_dn.py --learning_rate $learn --eval_epochs 5 --experiment_name "$learn" --multi_gpu --batch-size 12 --result_path ../results/joint_DN --epochs 60                                                                                                                
-done
