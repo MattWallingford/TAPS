9f8f62ac9b5e967a52dad8794f8b96ee5ad6bfe5
MattWallingford <mcw244@cornell.edu>
2022-10-11 11:49:41 -0700

    Organizing files

---
+++ scripts/sequential_dn.sh
@@ -0,0 +1,10 @@
+datasets="sketch clipart infograph painting quickdraw real"
+  
+
+for dataset in $datasets; do
+    for learn in .005; do
+        for lam in .5 1; do 
+                python fine_tune_freeze_dn.py --learning_rate $learn --eval_epochs 5 --experiment_name "$dataset-$learn-$lam" --multi_gpu --dataset $dataset --batch-size 32 --result_path ../results/DomainNet_r34_sequential --epochs 30 --lam $lam 
+        done                                                                                                                          
+    done
+done
